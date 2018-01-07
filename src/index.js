b070e75c327951fbe8efd690927172aaa1a336db
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-07 16:20:18 -0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import Provider from "./components/Provider";
-import TodoApp from "./App";
-import todoApp from "./reducers";
-import { loadState, saveState } from "./localStorage";
+import { render } from "react-dom";
+import Root from "./components/Root";
+import configureStore from "./configureStore";
 
-const persistedState = loadState();
+const store = configureStore();
 
-const store = createStore(
-  todoApp,
-  persistedState //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-store.subscribe(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-});
-ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
-  document.getElementById("root")
-);
+render(<Root store={store} />, document.getElementById("root"));
 
 // registerServiceWorker();
