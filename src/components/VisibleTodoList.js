e2428743d2819581ec8a5b247d3748eea8b4f25a
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-08 00:11:58 -0300

    add logger

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import TodoList from "./TodoList";
 import { getVisibleTodos } from "../reducers";
--- src/configureStore.js
+++ src/configureStore.js
@@ -3,12 +3,27 @@ import { loadState, saveState } from "./localStorage";
 import throttle from "lodash/throttle";
 import todoApp from "./reducers/index";
 
+const addLoggintToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  return action => {
+    console.group(action.type);
+    console.log("%c prev state", "color: gray", store.getState());
+    console.log("%c action", "color: blue", action);
+    const returnValue = rawDispatch(action);
+    console.log("%c next state", "color: green", store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(
     todoApp,
     persistedState //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.dispatch = addLoggintToDispatch(store);
   store.subscribe(
     throttle(() => {
       saveState({
