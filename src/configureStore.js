339fe0f3eaced9e137b5bd8cd77f0a6ae1f27e41
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-08 01:03:37 -0300

    add redux middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,46 +1,58 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { loadState, saveState } from "./localStorage";
 import throttle from "lodash/throttle";
 import todoApp from "./reducers/index";
 
-const addLoggintToDispatch = store => {
-  const rawDispatch = store.dispatch;
+const logger = store => next => {
   if (!console.group) {
     // not available in some browsers
-    return rawDispatch;
+    return next;
   }
   return action => {
     console.group(action.type);
     console.log("%c prev state", "color: gray", store.getState());
     console.log("%c action", "color: blue", action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log("%c next state", "color: green", store.getState());
     console.groupEnd(action.type);
     return returnValue;
   };
 };
 
-const addPromiseSupportToDispatch = store => {
-  const rawDispatch = store.dispatch;
-  return action => {
-    // chek if the action is a promise though
-    if (typeof action.then === "function") {
-      // the action should return an action object
-      return action.then(rawDispatch);
-    }
-    return rawDispatch;
-  };
+const promise = store => next => action => {
+  // chek if the action is a promise though
+  if (typeof action.then === "function") {
+    // the action should return an action object
+    return action.then(next);
+  }
+  return next(action);
 };
+
+// const wrapDispatchWithMiddlewares = (store, middlewares) => {
+//   middlewares
+//     .slice()
+//     .reverse()
+//     .forEach(middleware => {
+//       store.dispatch = middleware(store)(store.dispatch);
+//     });
+// };
 const configureStore = () => {
   const persistedState = loadState();
+
+  // the order of the middlewares are the order
+  // in which the actions progress throught them
+  const middlewares = [promise];
+  if (process.env.NODE_ENV !== "production") {
+    middlewares.push(logger);
+  }
+  // wrapDispatchWithMiddlewares(store, middlewares);
+
   const store = createStore(
     todoApp,
-    persistedState //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    persistedState, //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middlewares)
   );
-  if (process.env.NODE_ENV !== "production") {
-    store.dispatch = addLoggintToDispatch(store);
-  }
-  store.dispatch = addPromiseSupportToDispatch(store);
+
   store.subscribe(
     throttle(() => {
       saveState({
