75496ec2ab63208aecc453a11793fd06e6f4258b
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-01-07 22:58:43 -0300

    add validation in the action creators

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,10 +1,11 @@
 import { v4 } from "node-uuid";
 
 export const addTodoAction = text => {
+  if (text.trim().length === 0) return { type: "INVALID_INPUT" };
   return {
     type: "ADD_TODO",
     id: v4(),
-    text
+    text: text.trim()
   };
 };
 
@@ -29,9 +30,10 @@ export const toggleAllAction = () => {
 };
 
 export const editTodoAction = (id, text) => {
+  if (text.trim().length === 0) return { type: "INVALID_INPUT" };
   return {
     type: "EDIT_TODO",
     id,
-    text
+    text: text.trim()
   };
 };
